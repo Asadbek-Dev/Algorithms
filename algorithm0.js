9eb3a6379a674e27d03f46d6a64cfb664d9e553d
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-12-21 22:59:54 +0500

    changed

--- algorithm0.js
+++ algorithm0.js
@@ -1,9 +1,13 @@
-const sumUpTo= n => {
-    var total = 0;
-    for(let i = 1;i <= n; i++){
-        total +=i;
-    }
-    return total;
+// const sumUpTo= n => {
+//     var total = 0;
+//     for(let i = 1;i <= n; i++){
+//         total +=i;
+//     }
+//     return total;
+// }
+
+const sumUpTo = n =>{
+    return(n * (n + 1))/2;
 }
 
 sumUpTo(5)

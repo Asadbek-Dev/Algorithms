3319cdf1f1437fde2ef2dbaa1dd871c427ff4f7b
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-12-22 22:42:50 +0500

    changed

--- algorithm0.js
+++ algorithm0.js
@@ -6,8 +6,18 @@
 //     return total;
 // }
 
-const sumUpTo = n =>{
+const sumUpToV2 = n =>{
     return(n * (n + 1))/2;
 }
 
-sumUpTo(5)
+let t1,t2;
+
+t1=Date.now();
+sumUpToV2(2500000000);
+t2=Date.now();
+console.log(`${(t2-t1)/1000} seconds`);
+
+t1=Date.now();
+sumUpToV2(2500000000);
+t2=Date.now();
+console.log(`${(t2-t1)/1000} seconds`);

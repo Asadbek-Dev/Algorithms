2363615fccff36af53f4ebc71747ce09dbbcd6fb
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-12-24 22:12:01 +0500

    added comment

--- algorithm0.js
+++ algorithm0.js
@@ -1,16 +1,17 @@
-function sumAll(arr){
-    let min,max;
-    if(arr[0]>arr[1]){
-        max=arr[0];
-        min=arr[1];
-    }else{
-        max=arr[1];
-        min=arr[0];
-    }
-    let sum=0;
-    for(let i=min;i<=max;i++){
-        sum+=i;
-    }
-    return sum;
-}
-console.log(sumAll([4,4]))
+//    Sum all numbers in a range
+// function sumAll(arr){
+//     let min,max;
+//     if(arr[0]>arr[1]){
+//         max=arr[0];
+//         min=arr[1];
+//     }else{
+//         max=arr[1];
+//         min=arr[0];
+//     }
+//     let sum=0;
+//     for(let i=min;i<=max;i++){
+//         sum+=i;
+//     }
+//     return sum;
+// }
+// console.log(sumAll([4,4]))

50a422c3db64c0a201ad7ed57aed58ac0277ed7c
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-12-24 22:10:50 +0500

    completed

--- algorithm0.js
+++ algorithm0.js
@@ -1,23 +1,16 @@
-// const sumUpTo= n => {
-//     var total = 0;
-//     for(let i = 1;i <= n; i++){
-//         total +=i;
-//     }
-//     return total;
-// }
-
-const sumUpToV2 = n =>{
-    return(n * (n + 1))/2;
+function sumAll(arr){
+    let min,max;
+    if(arr[0]>arr[1]){
+        max=arr[0];
+        min=arr[1];
+    }else{
+        max=arr[1];
+        min=arr[0];
+    }
+    let sum=0;
+    for(let i=min;i<=max;i++){
+        sum+=i;
+    }
+    return sum;
 }
-
-let t1,t2;
-
-t1=Date.now();
-sumUpToV2(2500000000);
-t2=Date.now();
-console.log(`${(t2-t1)/1000} seconds`);
-
-t1=Date.now();
-sumUpToV2(2500000000);
-t2=Date.now();
-console.log(`${(t2-t1)/1000} seconds`);
+console.log(sumAll([4,4]))

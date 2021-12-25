1a7a0ce1662f4fd6c699a9b4425a3a076a8637f2
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-12-25 20:28:18 +0500

    changed

--- algorithm0.js
+++ algorithm0.js
@@ -1,17 +1,11 @@
 //    Sum all numbers in a range
-// function sumAll(arr){
-//     let min,max;
-//     if(arr[0]>arr[1]){
-//         max=arr[0];
-//         min=arr[1];
-//     }else{
-//         max=arr[1];
-//         min=arr[0];
-//     }
-//     let sum=0;
-//     for(let i=min;i<=max;i++){
-//         sum+=i;
-//     }
-//     return sum;
-// }
-// console.log(sumAll([4,4]))
+function sumAll(arr){
+    const min=arr[0]<arr[1] ? arr[0]:arr[1];
+    const max=arr[0]>arr[1] ? arr[0]:arr[1];
+    let sum=0;
+    for(let i=min;i<=max;i++){
+        sum+=i;
+    }
+    return sum;
+}
+console.log(sumAll([4,1]))

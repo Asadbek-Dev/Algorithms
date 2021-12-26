8ba2202646ec79c066aa0cbcf4d41068d46eed44
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-12-26 22:51:03 +0500

    changed

--- algorithm0.js
+++ algorithm0.js
@@ -3,6 +3,7 @@ function sumAll(arr){
     const min=arr[0]<arr[1] ? arr[0]:arr[1];
     const max=arr[0]>arr[1] ? arr[0]:arr[1];
     let sum=0;
+    
     for(let i=min;i<=max;i++){
         sum+=i;
     }
